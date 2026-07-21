0438ee96ffc7398d1ef062039db676673d415e9e
aadi0325-cpu <modelaadi98@gmail.com>
2026-07-21 04:25:41 +0000

    even code

---
+++ even.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout<< "Enter integer number : ";
+    cin>> a;
+    if(a%2==0) {
+        cout<< "no is even\n";
+    } else {
+        cout<< "no is odd\n";
+    }
+}
