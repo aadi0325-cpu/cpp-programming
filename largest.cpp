1ece147d6dbaad9aad3de9ca09e8da07f79a39e6
aadi0325-cpu <modelaadi98@gmail.com>
2026-07-21 04:48:41 +0000

    largest number

--- largest.cpp
+++ largest.cpp
@@ -1,1 +1,20 @@
-#include
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout<< "Enter any three numbers : \n";
+    cin>> a>> b>> c;
+
+    if (a >= b && a >= c) {
+        cout << "The largest number is: " << a << endl;
+    } 
+    else if (b >= a && b >= c) {
+        cout << "The largest number is: " << b << endl;
+    } 
+    else {
+        cout << "The largest number is: " << c << endl;
+    }
+
+}
