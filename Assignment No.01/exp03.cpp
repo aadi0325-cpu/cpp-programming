df56c6038c94f8f941e5d75bd7f6eb91523d9f0a
aadi0325-cpu <modelaadi98@gmail.com>
2026-08-11 05:24:33 +0000

    To perform addition, subtraction, multiplication, division and mod of two numbers.

--- Assignment No.01/exp03.cpp
+++ Assignment No.01/exp03.cpp
@@ -10,18 +10,18 @@ int a, b , sum, sub, mul, div, mod;
 cout << "Enter any two number :  ";
 cin >> a >> b;
 sum = a + b;
-cout<<"Addition of two no: \n"<<sum;
+cout<<"Addition of two no : \n"<<sum;
 
 sub = a - b;
-cout<<"Substraction of two no: \n"<<sub;
+cout<<"Substraction of two no : \n"<<sub;
 
 mul = a * b;
-cout<<"Multiplication of two no: \n"<<mul;
+cout<<"Multiplication of two no : \n"<<mul;
 
 div = a / b;
-cout<<"Division of two no: \n"<<div;
+cout<<"Division of two no : \n"<<div;
 
 mod = a % b;
-cout<<"Mod of two no: \n"<<mod;
+cout<<"Mod of two no : \n"<<mod;
 
 }
