03ead7c92439870c54e3bac2d84366a46b278911
aadi0325-cpu <modelaadi98@gmail.com>
2026-08-11 04:42:18 +0000

    To claculate the numerator and denominator by using cpp language.

--- Assignment No.02/exp22.cpp
+++ Assignment No.02/exp22.cpp
@@ -1,1 +1,48 @@
-// To 
+// To claculate the numerator and denominator by using cpp language.
+
+#include <iostream>
+using namespace std;
+
+class Fraction
+{
+private:
+    int numerator;
+    int denominator;
+
+public:
+    void input()
+    {
+        cout << "Enter numerator: ";
+        cin >> numerator;
+
+        cout << "Enter denominator: ";
+        cin >> denominator;
+    }
+
+    void display()
+    {
+        cout << "\nNumerator = " << numerator;
+        cout << "\nDenominator = " << denominator;
+
+        if (denominator != 0)
+        {
+            cout << "\nFraction = " << numerator << "/" << denominator;
+        }
+        else
+        {
+            cout << "\nError: Denominator cannot be zero.";
+        }
+    }
+};
+
+int main()
+{
+    Fraction f;
+
+    cout << "Enter the values of a fraction:" << endl;
+
+    f.input();
+
+    f.display();
+
+}
