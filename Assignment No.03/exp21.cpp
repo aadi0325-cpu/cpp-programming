f1724e26e29628b6a0c7b21b27ee4f24429126b8
aadi0325-cpu <modelaadi98@gmail.com>
2026-08-11 05:05:30 +0000

    To find the addition & substraction of complex number using cpp language.

---
+++ Assignment No.03/exp21.cpp
@@ -0,0 +1,78 @@
+// To find the addition & substraction of complex number using cpp language. 
+
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+private:
+    float real;
+    float imag;
+
+public:
+    void input()
+    {
+        cout << "Enter real part: ";
+        cin >> real;
+
+        cout << "Enter imaginary part: ";
+        cin >> imag;
+    }
+
+    Complex add(Complex c)
+    {
+        Complex result;
+
+        result.real = real + c.real;
+        result.imag = imag + c.imag;
+
+        return result;
+    }
+
+    Complex subtract(Complex c)
+    {
+        Complex result;
+
+        result.real = real - c.real;
+        result.imag = imag - c.imag;
+
+        return result;
+    }
+
+    void display()
+    {
+        if (imag >= 0)
+            cout << real << " + " << imag << "i";
+        else
+            cout << real << " - " << -imag << "i";
+    }
+};
+
+int main()
+{
+    Complex c1, c2, addition, subtraction;
+
+    cout << "Enter First Complex Number:" << endl;
+    c1.input();
+
+    cout << "\nEnter Second Complex Number:" << endl;
+    c2.input();
+
+    addition = c1.add(c2);
+
+    subtraction = c1.subtract(c2);
+
+    // Display first number
+    cout << "\nFirst Complex Number: ";
+    c1.display();
+
+    cout << "\nSecond Complex Number: ";
+    c2.display();
+
+    cout << "\n\nAddition: ";
+    addition.display();
+
+    cout << "\nSubtraction: ";
+    subtraction.display();
+
+}
